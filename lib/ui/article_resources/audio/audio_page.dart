0a497acf10fa0a24d1c8fb1375f860349f95a4b3
rushabh-gowda <rushabh.gowda@sidhiconsulting.com>
2020-05-23 02:00:06 -0400

    - Added audio recording and delete file

--- lib/ui/article_resources/audio/audio_page.dart
+++ lib/ui/article_resources/audio/audio_page.dart
@@ -1,6 +1,10 @@
+import 'dart:io';
+import 'dart:math';
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_i18n/flutter_i18n.dart';
+import 'package:path_provider/path_provider.dart';
 import 'package:repairservices/res/R.dart';
 import 'package:repairservices/ui/0_base/bloc_state.dart';
 import 'package:repairservices/ui/0_base/navigation_utils.dart';
@@ -10,6 +14,7 @@ import 'package:repairservices/ui/1_tx_widgets/tx_icon_button_widget.dart';
 import 'package:repairservices/ui/1_tx_widgets/tx_main_bar_widget.dart';
 import 'package:repairservices/ui/1_tx_widgets/tx_text_widget.dart';
 import 'package:repairservices/ui/article_resources/audio/audio_bloc.dart';
+import 'package:audio_recorder/audio_recorder.dart';
 
 class AudioPage extends StatefulWidget {
   final String filePath;
@@ -21,8 +26,44 @@ class AudioPage extends StatefulWidget {
 }
 
 class _AudioState extends StateWithBloC<AudioPage, AudioBloC> {
+  Recording _recording = new Recording();
+  bool _isRecording = false; 
+  String _savedFilePath;
+
   void _navBack() {
     NavigationUtils.pop(context);
+  }
+
+  _start() async {
+    try {
+      if (await AudioRecorder.hasPermissions) {
+        print("Start recording");
+        bloc.audioPath().then((value) async => {
+          await AudioRecorder.start(path: value).then((value) async =>
+          await AudioRecorder.isRecording.then((value) => setState(() {
+            _recording = new Recording(duration: new Duration());
+            _isRecording = value;
+          })))
+        });
+      } else {
+        print("You must accept permissions");
+      }
+    } catch (e) {
+      print(e);
+    }
+  }
+
+  _stop() async {
+    var recording = await AudioRecorder.stop();
+    print("Stop recording: ${recording.path}");
+    bool isRecording = await AudioRecorder.isRecording;
+    File file = File(recording.path);
+    print("  File length: ${await file.length()} ");
+    setState(() {
+      _recording = recording;
+      _isRecording = isRecording;
+      _savedFilePath = file.path;
+    });
   }
 
   @override
@@ -51,7 +92,7 @@ class _AudioState extends StateWithBloC<AudioPage, AudioBloC> {
               TXDividerWidget(),
               Container(
                 width: double.infinity,
-                height: 300,
+                height: 350,
                 child: Column(
                   children: <Widget>[
                     TXIconButtonWidget(
@@ -64,13 +105,31 @@ class _AudioState extends StateWithBloC<AudioPage, AudioBloC> {
                       text: "00:00:00",
                     ),
                     SizedBox(
-                      height: 50,
+                      height: 10,
+                    ),
+                    TXButtonWidget(
+                      title: FlutterI18n.translate(context, 'Start recording'),
+                      textColor: Colors.white,
+                      mainColor: Colors.green,
+                      onPressed: _isRecording ? null : _start,
+                    ),
+                    SizedBox(
+                      height: 10,
+                    ),
+                    TXButtonWidget(
+                      title: FlutterI18n.translate(context, 'Stop recording'),
+                      textColor: Colors.white,
+                      mainColor: Colors.orangeAccent,
+                      onPressed: _isRecording ? _stop : null,
+                    ),
+                    SizedBox(
+                      height: 10,
                     ),
                     TXButtonWidget(
                       title: FlutterI18n.translate(context, 'Delete record'),
-                      onPressed: () {},
                       textColor: Colors.white,
                       mainColor: Colors.red,
+                      onPressed: () {  bloc.deleteAudio(_savedFilePath);},
                     )
                   ],
                 ),
@@ -81,4 +140,6 @@ class _AudioState extends StateWithBloC<AudioPage, AudioBloC> {
       ),
     );
   }
+
+
 }
